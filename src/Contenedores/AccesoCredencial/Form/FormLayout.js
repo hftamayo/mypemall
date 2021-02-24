b8678ee53b12dfdd4a70c71dd7daa8212fbd9b4d
hftamayo <hftamayo@gmail.com>
2021-02-23 21:03:04 -0600

    validate credentials: bug onreset button

--- src/Contenedores/AccesoCredencial/Form/FormLayout.js
+++ src/Contenedores/AccesoCredencial/Form/FormLayout.js
@@ -1,32 +1,63 @@
-import { Box, Button, Form, FormField, TextInput } from 'grommet';
+import { Box, Button, Form, FormField } from 'grommet';
 import React from 'react';
 //import { useParams } from 'react-router-dom';
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-function FormLayout({ onSubmit, currentValues }) {
-  const defaultValues = {
-    ...currentValues,
-  };
+function FormLayout({ onSubmit, defaultValues }) {
+  const formik = useFormik({
+    initialValues: {
+      ...defaultValues,
+      correoeCliente: "",
+      claveCliente: ""
+    },
+    validationSchema: Yup.object({
+      correoeCliente: Yup.string()
+      .email("Formato de correo invalido")
+      .required("Requerido"),
+      claveCliente: Yup.string()
+      .min(8, "minimo 8 caracteres")
+      .required("Requerido"),
+    }),
+
+    onSubmit: (values) => {
+      onSubmit(values);
+    }
+  });
+
   const [value, setValue] = React.useState(defaultValues);
   
   return (
     <Form
       value={value}
-      onChange={(nextValue, { touched }) => {
-        console.log('Change', nextValue, touched);
-        setValue(nextValue);
-      }}
-      onReset={() => setValue(defaultValues)}
-      onSubmit={(event) => {
-        console.log('Submit', event.value, event.touched);
-        onSubmit(event.value);
-      }}
+      onReset={() => setValue(formik.initialValues)}
+      onSubmit={formik.handleSubmit}
     >
       <FormField label="Correo Electronico" name="lblcorreoeCliente">
-        <TextInput name="correoeCliente" />
+        <input 
+        type="text"
+        name="correoeCliente"
+        value={formik.values.correoeCliente}
+        onChange={formik.handleChange}
+        />
+        {
+          formik.errors.correoeCliente && formik.touched.correoeCliente && (
+            <p style={{color:"red"}}>{formik.errors.correoeCliente}</p>
+          )}
       </FormField>
 
       <FormField label="Clave" name="lblclaveCliente">
-        <TextInput name="claveCliente" />
+      <input 
+        type="password"
+        name="claveCliente"
+        value={formik.values.claveCliente}
+        onChange={formik.handleChange}
+        />
+        {
+          formik.errors.claveCliente && formik.touched.claveCliente && (
+            <p style={{color:"red"}}>{formik.errors.claveCliente}</p>
+          )}
+
       </FormField>
 
       <Box direction="row" justify="between" margin={{ top: 'medium' }}>
